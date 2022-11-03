e16468ad5b6f0d036fd8e0246ba8ba6758d5eab9
Oshioke-Salaki <salaki1902@gmail.com>
2022-11-03 14:11:51 -0600

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Created a Guess the number game using vanilla Javascript
+
+Document Object Model (DOM) is the structure representation of html documents. It allows Javascript to access HTML elements and styles to manipulate them.
+
+DOM isn't a part of Javascript. DOM and DOM methods are part of web API's which are libraries that the browesrs implement that we can access from our Javascript code.
+
+Math.trunc is a method that cuts off the decimal part of a number.
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
+let score = Number(document.querySelector('.score').textContent);
+
+function clickss() {
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔️ No number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    } else if (guess > secretNumber) {
+        if (score > 0) {
+            document.querySelector('.message').textContent = '📈 Too high!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💥 You lost the game!';
+        }
+    } else if (guess < secretNumber) {
+        if (score > 0) {
+            document.querySelector('.message').textContent = '📉 Too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💥 You lost the game!';
+        }
+    }
+}
+
+document.querySelector('.check').addEventListener('click', clickss);
