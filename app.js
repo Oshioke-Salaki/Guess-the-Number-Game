8fbacf6c585349f8141fa94a07acc7823bc15028
Oshioke-Salaki <salaki1902@gmail.com>
2022-11-04 09:23:14 -0600

    Updated

--- app.js
+++ app.js
@@ -1,37 +1,66 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
-
-let score = Number(document.querySelector('.score').textContent);
+function displayMessage(message) {
+    document.querySelector('.message').textContent = message;
+}
 
 function clickss() {
     const guess = Number(document.querySelector('.guess').value);
 
     console.log(guess, typeof guess);
 
+    //When there is no input
     if (!guess) {
-        document.querySelector('.message').textContent = '⛔️ No number!';
+        displayMessage('⛔️ No number!');
+
+        //When player wins
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 Correct Number!';
-    } else if (guess > secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = '📈 Too high!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '💥 You lost the game!';
+        displayMessage('🎉 Correct Number!');
+
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+
+            document.querySelector('.highscore').textContent = highScore;
         }
-    } else if (guess < secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = '📉 Too low!';
+
+        //When guess is not correct
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess < secretNumber ? '📉 Too low!' : '📈 Too high!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = '💥 You lost the game!';
+            displayMessage('💥 You lost the game!');
+            document.querySelector('.score').textContent = 0;
         }
     }
 }
 
+function startAgain() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    score = 20;
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+
+    console.log(secretNumber);
+}
+
 document.querySelector('.check').addEventListener('click', clickss);
+
+document.querySelector('.again').addEventListener('click', startAgain);
